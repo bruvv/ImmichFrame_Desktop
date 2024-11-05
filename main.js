c101132ee3de2ae0e22319a8c6ec6c97e4c500ac
Rob Rogers <robrogers@zoominternet.net>
2024-11-05 17:11:22 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+let mainWindow;
+const urlFilePath = path.join(app.getPath('userData'), 'settings.txt');
+
+// Function to create the main window
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+		fullscreen: true, // Start in fullscreen mode
+        frame: false, // Remove the window frame (no title bar or menu)
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true,
+            webviewTag: true,
+        },
+    });
+
+    // Load the last saved URL or a default one
+    if (fs.existsSync(urlFilePath)) {
+        const savedUrl = fs.readFileSync(urlFilePath, 'utf-8');
+        mainWindow.loadFile('index.html').then(() => {
+            mainWindow.webContents.executeJavaScript(`document.getElementById('webview').setAttribute('src', '${savedUrl}');`);
+        });
+    } else {
+        mainWindow.loadFile('index.html');
+    }
+}
+
+// IPC listener to retrive URL
+ipcMain.handle('get-saved-url', async () => {
+    try {
+        const url = fs.readFileSync(urlFilePath, 'utf-8');
+        return url; // Return the saved URL
+    } catch (error) {        
+        return 'https://immichframe.github.io/ImmichFrame/'; // Return the default URL
+    }
+});
+
+// IPC listener to save URL
+ipcMain.on('save-url', (event, url) => {
+    fs.writeFileSync(urlFilePath, url, 'utf-8');
+});
+
+// IPC listener to quit
+ipcMain.on('quit-app', () => {
+    app.quit();
+});
+
+// Application lifecycle management
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
