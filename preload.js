ec881d90a4cb1bfb67c33023412ca17883d8b82f
Rob Rogers <robrogers@zoominternet.net>
2024-11-13 21:30:02 -0500

    intial Tauri commit

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-    sendButtonClick: (buttonName) => ipcRenderer.send('button-clicked', buttonName),
-});
